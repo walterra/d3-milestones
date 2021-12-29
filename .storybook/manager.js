78a86e5a8ad4704f1d829e9d234dc7f26ba0e7ac
Walter Rafelsberger <walter@elastic.co>
2021-12-29 15:45:16 +0100

    Display version in storybook title.

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,9 +1,11 @@
 import { addons } from '@storybook/addons';
 import { create } from '@storybook/theming';
 
+import p from '../package.json';
+
 addons.setConfig({
   theme: create({
     base: 'light',
-    brandTitle: 'd3-milestones',
+    brandTitle: `d3-milestones v${p.version}`,
   }),
 });
