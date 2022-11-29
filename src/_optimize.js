825998e1c38a66c9d28951d0f9b871954fac58e1
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2022-11-29 15:16:11 +0100

    wip: layout vertical offset

--- src/_optimize.js
+++ src/_optimize.js
@@ -1,8 +1,7 @@
 import * as dom from 'd3-selection';
 import { nest } from 'd3-collection';
 
-import { cssAboveClass } from './_css';
-// import { cssLastClass } from './_css';
+import { cssAboveClass, cssLastClass } from './_css';
 // import { getAttribute } from './_get_attribute';
 // import { getAvailableWidth } from './_get_available_width';
 // import { getNextGroupHeight } from './_get_next_group_height';
@@ -26,6 +25,9 @@ const getParentElement = (domElement) =>
 //   return distributionCheck !== 0;
 // };
 
+const LABEL_MIN_WIDTH = 60;
+const LABEL_MAX_WIDTH = 180;
+
 export const optimize = (
   aggregateFormatParse,
   distribution,
@@ -38,64 +40,153 @@ export const optimize = (
   widthAttribute,
   x
 ) => {
-  const nestedNodes = nest()
-    .key((d) => {
-      return dom.selectAll(d).data()[0].timelineIndex;
-    })
-    .entries(textMerge._groups);
+  // console.log('width', width);
+  let orangeCount = 1;
+  let iterations = 0;
 
-  // const nextCheck = distribution === 'top-bottom' ? 2 : 1;
+  while (orangeCount > 0 && iterations < 10) {
+    // debugger;
+    orangeCount = 0;
+    iterations++;
+    const nestedNodes = nest()
+      .key((d) => {
+        return dom.selectAll(d).data()[0].timelineIndex;
+      })
+      .entries(textMerge._groups);
 
-  for (const timeline of nestedNodes) {
-    // const timelineNodes = timeline.values.map((d) => d[0]);
+    for (const timeline of nestedNodes) {
+      const aboveNodes = timeline.values.filter((tn) =>
+        getParentElement(dom.select(tn[0])).classed(
+          `${cssAboveClass}-${orientation}`
+        )
+      );
 
-    const aboveNodes = timeline.values.filter((tn) =>
-      getParentElement(dom.select(tn[0])).classed(
-        `${cssAboveClass}-${orientation}`
-      )
-    );
+      const boundingsRects = Array(aboveNodes.length);
+      let totalHeight = 0;
+      let maxHeight = 0;
 
-    const startNodeIndex = Math.floor(aboveNodes.length / 2);
+      for (const [i, aboveNode] of aboveNodes.entries()) {
+        const item = dom.selectAll(aboveNode).data()[0];
+        const offset = x(aggregateFormatParse(item.key));
+        const boundingRect = getParentElement(dom.select(aboveNode[0]))
+          .node()
+          .getBoundingClientRect();
 
-    const offsets = Array(aboveNodes.length);
-    let totalHeight = 0;
+        totalHeight += boundingRect.height;
+        maxHeight = Math.max(maxHeight, boundingRect.height);
 
-    for (const [i, aboveNode] of aboveNodes.entries()) {
-      const item = dom.selectAll(aboveNode).data()[0];
-      const offset = x(aggregateFormatParse(item.key));
-      const rect = dom.selectAll(aboveNode).node().getBoundingClientRect();
-
-      totalHeight += rect.height;
-
-      offsets[i] = offset;
-    }
-
-    const bitmap = Array.from({ length: totalHeight }, () =>
-      Array.from({ length: width }, () => false)
-    );
-
-    for (const [i, offset] of offsets.entries()) {
-      if (i !== startNodeIndex) {
-        bitmap[bitmap.length - 1][offset] = true;
+        boundingRect.text = item.key;
+        boundingRect.index = i;
+        boundingRect.offset = offset;
+        boundingsRects[i] = boundingRect;
       }
-    }
 
-    const canvas = document.getElementById('bitmap');
-    dom.select(canvas).attr('width', width);
-    dom.select(canvas).attr('height', bitmap.length);
-    if (canvas.getContext) {
-      const ctx = canvas.getContext('2d');
+      const bitmap = Array.from({ length: totalHeight }, () =>
+        Array.from({ length: width }, () => false)
+      );
 
-      ctx.fillStyle = 'rgba(255,0,0,255)';
+      const canvas = document.getElementById('bitmap');
+      dom.select(canvas).attr('width', width);
+      dom.select(canvas).attr('height', bitmap.length);
+      if (canvas.getContext) {
+        const ctx = canvas.getContext('2d');
 
-      bitmap.forEach((row, yIndex) => {
-        row.forEach((pixel, xIndex) => {
-          // console.log('pixel', xIndex, yIndex, pixel);
-          if (pixel) {
-            ctx.fillRect(xIndex, yIndex, 1, 1);
+        const lowestGreen = boundingsRects.reduce((p, c) => {
+          const pHeight = p ? p.height : maxHeight;
+
+          if (pHeight < c.height) return p;
+
+          const leftEl = boundingsRects.find(
+            (d) => d.index === c.index - 1 && d.width < LABEL_MIN_WIDTH
+          );
+
+          const rightEl = boundingsRects.find(
+            (d) => d.index === c.index + 1 && d.width < LABEL_MIN_WIDTH
+          );
+
+          return leftEl !== undefined || rightEl !== undefined ? c : p;
+        }, undefined);
+
+        orangeCount = lowestGreen === undefined ? 0 : 1;
+
+        if (lowestGreen !== undefined) {
+          let lowestOrange;
+          let side;
+
+          if (lowestGreen.index > 0) {
+            lowestOrange = boundingsRects.find(
+              (d) =>
+                d.index === lowestGreen.index - 1 && d.width < LABEL_MIN_WIDTH
+            );
+            side = 'before';
           }
-        });
-      });
+          if (lowestGreen.index < boundingsRects.length) {
+            const checkOrange = boundingsRects.find(
+              (d) =>
+                d.index === lowestGreen.index + 1 && d.width < LABEL_MIN_WIDTH
+            );
+            if (
+              lowestOrange === undefined ||
+              (checkOrange !== undefined &&
+                lowestOrange.height > checkOrange.height)
+            ) {
+              lowestOrange = checkOrange;
+              side = 'after';
+            }
+          }
+
+          const loVolume = lowestOrange.width * lowestOrange.height;
+          const newHeight = loVolume / LABEL_MIN_WIDTH;
+          const newYOffset = lowestGreen.height + 2;
+          const newX =
+            side === 'before'
+              ? lowestOrange.offset
+              : lowestOrange.offset - LABEL_MIN_WIDTH - 2;
+
+          ctx.fillStyle = `rgba(192,0,128,1)`;
+          ctx.fillRect(
+            newX,
+            bitmap.length - newYOffset - newHeight,
+            LABEL_MIN_WIDTH,
+            newHeight
+          );
+
+          const backwards = side === 'after';
+          const paddingAbove =
+            orientation === 'horizontal' ? 'padding-bottom' : 'padding-right';
+          const paddingBelow =
+            orientation === 'horizontal' ? 'padding-top' : 'padding-left';
+          const padding = paddingAbove;
+
+          const domElement = getParentElement(
+            dom.select(aboveNodes[lowestOrange.index][0])
+          );
+
+          domElement.classed(cssLastClass, backwards);
+          domElement.style(padding, `${lowestGreen.height}px`);
+          domElement.style(widthAttribute, `${LABEL_MIN_WIDTH + 5}px`);
+          dom
+            .select(aboveNodes[lowestOrange.index][0])
+            .style(widthAttribute, `${LABEL_MIN_WIDTH}px`);
+        }
+
+        for (const rect of boundingsRects) {
+          const alpha = 0.3 + (rect.height / maxHeight) * 0.9;
+
+          if (rect.width >= LABEL_MIN_WIDTH) {
+            ctx.fillStyle = `rgba(0,192,128,${alpha})`;
+          } else {
+            orangeCount++;
+            ctx.fillStyle = `rgba(255,128,0,${alpha})`;
+          }
+          ctx.fillRect(
+            rect.offset,
+            bitmap.length - rect.height,
+            rect.width,
+            rect.height
+          );
+        }
+      }
     }
   }
 };
