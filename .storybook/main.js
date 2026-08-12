fe158eed857001cb8433ef67321afa776293f505
Walter M. Rafelsberger <walter@rafelsberger.at>
2026-08-12 08:58:26 +0200

    storybook v9

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,31 +5,12 @@ module.exports = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-docs',
-    '@storybook/addon-essentials',
-    '@storybook/addon-mdx-gfm',
-    '@storybook/addon-webpack5-compiler-babel',
     '@chromatic-com/storybook'
   ],
 
   framework: {
-    name: '@storybook/html-webpack5',
+    name: '@storybook/html-vite',
     options: {},
-  },
-
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.less$/,
-      use: [
-        { loader: 'style-loader' },
-        { loader: 'css-loader', options: { modules: false } },
-        {
-          loader: 'less-loader',
-          options: { lessOptions: { javascriptEnabled: true } },
-        },
-      ],
-    });
-    config.optimization.minimize = false;
-    return config;
   },
 
   docs: {},
