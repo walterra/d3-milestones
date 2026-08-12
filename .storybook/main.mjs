a275b08ee29db01575516c58bb7f415cfded8771
Walter M. Rafelsberger <walter@rafelsberger.at>
2026-08-12 09:12:16 +0200

    Upgrade Storybook to v10 (#130)
    ## Summary
    
    - upgrade Storybook from 8.6 to 10.5
    - migrate the HTML framework from Webpack to Vite
    - update Storybook manager and MDX imports for current package paths
    - convert the main Storybook configuration to explicit ESM
    
    ## Verification
    
    - `yarn build-storybook`

---
+++ .storybook/main.mjs
@@ -0,0 +1,17 @@
+export default {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  staticDirs: ['../src/stories/assets'],
+
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-docs',
+    '@chromatic-com/storybook'
+  ],
+
+  framework: {
+    name: '@storybook/html-vite',
+    options: {},
+  },
+
+  docs: {},
+};
